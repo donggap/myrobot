3f63c02662a434fc167e7b07d5a07036250209ff
donggap <dong.0000000000@gmail.com>
2016-09-30 15:27:33 +0800

    edit splash

--- MyApp/app/src/main/java/com/mycompany/myapp/SplashActivity.java
+++ MyApp/app/src/main/java/com/mycompany/myapp/SplashActivity.java
@@ -1,5 +1,26 @@
-package com.mycompany.myapp;
+package com.danielBR.myrobot;
 
-public class SplashActivity
+import android.app.*;
+import android.os.*;
+import android.content.*;
+
+public class SplashActivity extends Activity 
 {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        
+        new Handler().postDelayed(new Runnable(){
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);//jump MainActivity
+                SplashActivity.this.finish();//finish SplashActivity
+            }
+        }, 3000);
+    }
 }
